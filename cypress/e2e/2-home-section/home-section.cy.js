76f4cb84b9ad3cbaf23788c1ab6be7da2d67bf2b
Bobana Simikic <bobana.simikic@gmail.com>
2025-03-16 13:06:42 +0100

    Verify elements in cart

--- cypress/e2e/2-home-section/home-section.cy.js
+++ cypress/e2e/2-home-section/home-section.cy.js
@@ -11,4 +11,29 @@ describe('Home Section', () => {
     it('verify visibility of home screen elements', () => {
         homePage.verifyHomePageElements();
     });
+
+    it('verify successfully added item to the cart', () => {
+        homePage.verifyElementInCart();
+    })
+    it('verify successfully added first item to the cart', () => {
+        homePage.verifyElement1InCart();
+    })
+    it('verify successfully added third item to the cart', () => {
+        homePage.verifyElement3InCart();
+    })
+    it('verify successfully added 4. item to the cart', () => {
+        homePage.verifyElement4InCart();
+    })
+    it ('verify successfully added 5. item to the cart', () => {
+        homePage.verifyElement5InCart();
+    })
+    it('verify successfully added 6. item to the cart', () => {
+        homePage.verifyElement6InCart();
+    })
+    it('verify successfully added two items to the cart', () => {
+        homePage.verifyFirstAndThirdElementInCart();
+    })
+    it('verify successfully added two random items to the cart', () => {
+        homePage.verifyRandomElementsInCart();
+    })
 });
--- cypress/support/pages/homePage.js
+++ cypress/support/pages/homePage.js
@@ -49,7 +49,8 @@ export class HomePage {
         cy.origin('https://saucelabs.com/', () => {
             cy.url().should('include', 'saucelabs');
         });
-//        cy.get(homePageLocators.aboutSauceBtn).should('be.visible').and('contain.text', 'Test it all. Free')
+        //        cy.get(homePageLocators.aboutSauceBtn).should('be.visible').and('contain.text', 'Test it all. Free')
+
 
     }
     userCanLogout() {
@@ -58,12 +59,161 @@ export class HomePage {
         cy.url().should('eq', 'https://www.saucedemo.com/')
         cy.get(loginPageLocators.loginButton).should('be.visible')
     }
+
     resetApp() {
         cy.get(homePageLocators.burgerMenuBtn).click();
         cy.get(homePageLocators.resetAppState).click();
         cy.url().should('eq', 'https://www.saucedemo.com/inventory.html');
 
     }
+    verifyElementInCart() {
+        cy.get(homePageLocators.itemPrice)
+            .eq(1)  // Selektuj drugi element u nizu
+            .invoke('text')  // Uzmi njegov tekst
+            .then((text) => {
+                cy.log(text) // Ispisi text u konzoli
+                cy.get(homePageLocators.addToCartButton).click()
+                cy.get(homePageLocators.cartButton).click()
+                cy.get(homePageLocators.itemInTheCart)
+                    .should('contain', text); // Verifikacija da je text iznad u korpi
+            });
+    }
+         addToCart(index) {
+        cy.get('[data-test="inventory-item"]')
+          .eq(index)  // Selektuje proizvod na osnovu indeksa
+          .find('button')  // Nalazi dugme unutar tog proizvoda
+          .click();  // Klikne na dugme "Add to Cart"
+      }
+    verifyElement1InCart() {
+        cy.get(homePageLocators.itemPrice)
+            .eq(0)  // Selektuj prvi element u nizu
+            .invoke('text')  // Uzmi njegov tekst
+            .then((text) => {
+                cy.log(text) // Ispisi text u konzoli
+                this.addToCart(0)
+                cy.get(homePageLocators.cartButton).click()
+                cy.get(homePageLocators.itemInTheCart)
+                    .should('contain', text); // Verifikacija da je text iznad u korpi
+            });
+    }
+
+    verifyElement3InCart() {
+        cy.get(homePageLocators.itemPrice)
+            .eq(2)  // Selektuj treci element u nizu
+            .invoke('text')  // Uzmi njegov tekst
+            .then((text) => {
+                cy.log(text) // Ispisi text u konzoli
+                this.addToCart(2)
+                cy.get(homePageLocators.cartButton).click()
+                cy.get(homePageLocators.itemInTheCart)
+                    .should('contain', text); // Verifikacija da je text iznad u korpi
+            });
+
+    }
+    verifyElement4InCart() {
+        cy.get(homePageLocators.itemPrice)
+            .eq(3)  // Selektuj cetvrti element u nizu
+            .invoke('text')  // Uzmi njegov tekst
+            .then((text) => {
+                cy.log(text) // Ispisi text u konzoli
+                this.addToCart(3)
+                cy.get(homePageLocators.cartButton).click()
+                cy.get(homePageLocators.itemInTheCart)
+                    .should('contain', text); // Verifikacija da je text iznad u korpi
+            });
+
+    }
+    verifyElement5InCart() {
+        cy.scrollTo('bottom');
+        cy.get(homePageLocators.itemPrice)
+            .eq(4)  // Selektuj prvi element u nizu
+            .invoke('text')  // Uzmi njegov tekst
+            .then((text) => {
+                cy.log(text) // Ispisi text u konzoli
+                this.addToCart(4)
+                cy.get(homePageLocators.cartButton).click()
+                cy.get(homePageLocators.itemInTheCart)
+                    .should('contain', text); // Verifikacija da je text iznad u korpi
+            });
+    }
+    verifyElement6InCart() {
+        cy.scrollTo('bottom')    
+        cy.get(homePageLocators.itemPrice)
+            .eq(5)  // Selektuj prvi element u nizu
+            .invoke('text')  // Uzmi njegov tekst
+            .then((text) => {
+                cy.log(text) // Ispisi text u konzoli
+                this.addToCart(5)
+                cy.get(homePageLocators.cartButton).click()
+                cy.get(homePageLocators.itemInTheCart)
+                    .should('contain', text); // Verifikacija da je text iznad u korpi
+            });
+    }
+
+     addMultipleToCart(elements) {
+        elements.forEach((index) => {
+          cy.get('[data-test="inventory-item"]')
+            .eq(index)  // Selektuje proizvod na osnovu indeksa
+            .find('button')  // Nalazi dugme unutar tog proizvoda
+            .click();  // Klikne na dugme "Add to Cart"
+        });
+    }
+
+    verifyFirstAndThirdElementInCart() {
+        const selectedIndexes = [0, 2]; // Indeksi za prvi i treći element
+        const selectedItems = []; // Čuva tekst proizvoda
+
+        selectedIndexes.forEach((index) => {
+            cy.get(homePageLocators.itemPrice)
+                .eq(index)
+                .invoke("text")
+                .then((text) => {
+                    selectedItems.push(text); // Čuvamo tekst proizvoda
+                    cy.log(`Dodajem u korpu: ${text}`);
+                });
+        });
+          // Klik na dugme "Add to Cart" za odabrane elemente
+          this.addMultipleToCart(selectedIndexes)
+        // Otvaramo korpu i proveravamo da su proizvodi tu
+        cy.get(homePageLocators.cartButton).click();
+
+        selectedItems.forEach((item) => {
+            cy.get(homePageLocators.itemInTheCart).should("contain", item);
+        });
+
+
+    }
+
+    verifyRandomElementsInCart() {
+        cy.get(homePageLocators.itemPrice).then((items) => {
+            // Uzimamo dva nasumična indeksa
+            const randomIndexes = Cypress._.sampleSize([...Array(items.length).keys()], 2);
+
+            // Čuvamo vrednosti nasumično izabranih elemenata
+            const selectedItems = [];
+
+            // Iteracija kroz dva random elementa
+            randomIndexes.forEach((index) => {
+                cy.get(homePageLocators.itemPrice)
+                    .eq(index)
+                    .invoke("text")
+                    .then((text) => {
+                        selectedItems.push(text); // Dodajemo tekst proizvoda u niz
+                        cy.log(`Dodajem u korpu: ${text}`);
+                    });
+            });
+              // Klik na dugme "Add to Cart"
+              this.addMultipleToCart(randomIndexes)
+
+            // Otvori korpu i proveri da su proizvodi tu
+            cy.get(homePageLocators.cartButton).click();
+
+            selectedItems.forEach((item) => {
+                cy.get(homePageLocators.itemInTheCart).should("contain", item);
+            });
+        });
+    }
 }
+
 
 export const homePage = new HomePage();
